0bec3e71f6342b405e52681574373ac2bf44072b
4anth0ny1 <acatullo4@gmail.com>
2021-03-18 21:14:39 -0500

    Done.

---
+++ db/migrate/01_create_users.rb
@@ -0,0 +1,11 @@
+# db/migrate/01_create_users.rb
+
+class CreateUsers < ActiveRecord::Migration
+    def change
+      create_table :users do |t|
+        t.string :name
+        t.string :email
+        t.string :password
+      end
+    end
+  end 
